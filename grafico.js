81a8bfd82f8250b00732e40ed01d8bf92ba2aa0e
Eduardo2369 <161334578+Eduardo2369@users.noreply.github.com>
2024-10-16 08:16:43 -0300

    Add files via upload

--- grafico.js
+++ grafico.js
@@ -1,3 +1,6 @@
 // gráfico de Pizza
 const ctxPizza = document.getElementById('graficoPizza').getContext('2d');
 const graficoPizza = new Chart(ctxPizza, {
+    type: 'pie',
+    data: {
+        labels: []
